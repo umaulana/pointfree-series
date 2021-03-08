c491d79f9230e08a67d654c852fd43869861025f
umam <umam2r83@gmail.com>
2021-03-08 23:46:46 +0700

    Side Effects

---
+++ Episode-2/Pointfree.playground/Sources/Utils.swift
@@ -0,0 +1,46 @@
+// NB: `@_exported` will make foundation available in our playgrounds
+@_exported import Foundation
+import XCTest
+
+
+@discardableResult
+public func assertEqual<A: Equatable>(_ lhs: A, _ rhs: A) -> String {
+    return lhs == rhs ? "✅" : "❌"
+}
+
+@discardableResult
+public func assertEqual<A: Equatable, B: Equatable>(_ lhs: (A, B), _ rhs: (A, B)) -> String {
+    return lhs == rhs ? "✅" : "❌"
+}
+
+public var __: Void {
+    return print("--")
+}
+
+precedencegroup ForwardApplication {
+    associativity: left
+}
+infix operator |>: ForwardApplication
+public func |> <A, B>(_ x: A, f: (A) -> B) -> B {
+    return f(x)
+}
+
+precedencegroup ForwardComposition {
+    associativity: left
+    higherThan: ForwardApplication
+}
+
+infix operator >>>: ForwardComposition
+public func >>> <A, B, C>(_ f: @escaping  (A) -> B, g: @escaping  (B) -> C) -> (A) -> C {
+    return { a in
+        g(f(a))
+    }
+}
+
+public func incr(_ x: Int) -> Int {
+    return x + 1
+}
+
+public func square(_ x: Int) -> Int {
+    return x * x
+}
